ced78959b86ffaffc45aedc02b2e4988c1ccca5d
Kyron <kyron@Kyrons-MacBook-Pro.local>
2023-09-16 23:00:28 +0100

    data types lesson

---
+++ Data Types/Data Types/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string phrase = "kyron is cool";
+char grade = 'A';
+int age = 30;
+double money = 1.09283;
+bool passed = true;
+
+Console.WriteLine(phrase + grade + age + money + passed);
