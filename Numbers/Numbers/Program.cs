47544df4b365db8bc1b76111df6ed46bbf989cb2
Kyron <kyron@Kyrons-MacBook-Pro.local>
2023-09-16 23:19:29 +0100

    numbers lesson

---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num = 6;
+num++;
+
+Console.WriteLine(5 + 8);
+Console.WriteLine(5 + 8 * 3);
+Console.WriteLine(5 + 8.1);
+Console.WriteLine(5 / 2);
+Console.WriteLine(5.0 / 2.0);
+
+Console.WriteLine(num);
+num--;
+Console.WriteLine(num);
+Console.WriteLine(Math.PI);
+Console.WriteLine(Math.Abs(-40));
+Console.WriteLine(Math.Pow(3,3));
+Console.WriteLine(Math.Sqrt(9));
+Console.WriteLine(Math.Max(4 , 90));
+Console.WriteLine(Math.Min(4 , 90));
+Console.WriteLine(Math.Round(5.2));
+
