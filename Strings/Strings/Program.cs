bb91016353c8ff27487f9146317a017a7f47ad17
Kyron <kyron@Kyrons-MacBook-Pro.local>
2023-09-16 23:11:33 +0100

    strings lesson

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+string phrase = "tester text";
+
+Console.WriteLine(phrase.Length);
+Console.WriteLine(phrase.ToUpper());
+Console.WriteLine(phrase.ToLower());
+Console.WriteLine(phrase.Contains("text"));
+Console.WriteLine(phrase.Contains("pancakes"));
+Console.WriteLine(phrase[0]);
+Console.WriteLine(phrase.IndexOf("text"));
+Console.WriteLine(phrase.IndexOf("x"));
+Console.WriteLine(phrase.Substring(7));
+Console.WriteLine(phrase.Substring(0,6));
