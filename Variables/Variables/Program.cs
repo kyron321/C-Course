9988e1e4e9f224725bdd003ea2db2a5480dd9b54
Kyron <kyron@Kyrons-MacBook-Pro.local>
2023-09-16 22:51:36 +0100

    variables lesson

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string characterName = "Kyron";
+int characterAge = 21;
+Console.WriteLine(characterName + " is aged " + characterAge);
+
+characterName = "Clayton";
+characterAge = 19;
+Console.WriteLine(characterName + " is aged " + characterAge);
+
