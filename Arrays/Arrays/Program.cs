46775710a0f45f216e5f2c712aee0f13f75a16e9
Kyron <kyron@Kyrons-MacBook-Pro.local>
2023-09-17 08:56:31 +0100

    arrays lesson

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int [] numbers = {10, 20, 30};
+string [] names = new string[1];
+names[0] = "kyron";
+
+Console.WriteLine(numbers[0]);
+
+numbers[0] = 1;
+Console.WriteLine(numbers[0]);
+
