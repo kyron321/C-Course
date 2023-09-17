fb215431a56a887f11f35584c189d0366409aa4e
Kyron <kyron@Kyrons-MacBook-Pro.local>
2023-09-17 08:44:29 +0100

    building a calculator lesson

---
+++ Building A Calculator/Building A Calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter a number ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter another number ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(num1+num2);
+
