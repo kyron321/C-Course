41b5bc1b4c0b57420bf9ead103ba737cf2f7d530
Kyron <kyron@Kyrons-MacBook-Pro.local>
2023-09-17 09:05:18 +0100

    methods lesson

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Methdods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("what is your name? ");
+            string name = Console.ReadLine();
+            SayHi(name);
+        }
+        static void SayHi(string name){
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
