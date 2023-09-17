9bae576196f8f6c489c71fecfcffdabe83b8750a
Kyron <kyron@Kyrons-MacBook-Pro.local>
2023-09-17 08:36:23 +0100

    getting user input lesson

---
+++ Getting User Input/Getting User Input/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("What is your name? ");
+string name = Console.ReadLine();
+Console.Write("What is your age? ");
+string age = Console.ReadLine();
+Console.WriteLine("Hello " + name); 
+Console.WriteLine("You are  " + age); 
