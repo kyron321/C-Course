fe66190b9a8c435e022f6b5d55c88e5a2134e66e
Kyron <kyron@Kyrons-MacBook-Pro.local>
2023-09-17 09:16:11 +0100

    if statements lesson

---
+++ If Statements/If Statements/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool isMale = true;
+bool isTall = true;
+
+if (isMale && isTall){
+    Console.WriteLine("You are a tall man");
+} else if (isMale && !isTall) {
+        Console.WriteLine("You are a small man");
+} else if (!isMale && isTall){
+        Console.WriteLine("You are a tall woman");
+} else {
+        Console.WriteLine("You are a small woman");
+}
