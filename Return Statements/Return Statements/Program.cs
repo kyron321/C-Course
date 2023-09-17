b6ac2207620eb4054ab507bd0352ab30d8a5d3b6
Kyron <kyron@Kyrons-MacBook-Pro.local>
2023-09-17 09:09:17 +0100

    return statements lesson

---
+++ Return Statements/Return Statements/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(cube(3));
+
+static int cube(int num){
+    int result = num*num*num;
+    return result;
+}
